a66630a81eb69f541ed795e70a2b887c06b34a0e
matrox211 <98254409+matrox211@users.noreply.github.com>
2022-01-23 16:13:14 +0700

    add all files of project
    hi สร้างจุดบันทึก แต่ยังไม่ขึ้น server

---
+++ app.js
@@ -0,0 +1,16 @@
+//run main app (app.js or server.js or whatever)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(port,()=>{
+    //console.log("Listening on port %d",port)
+    //console.log("Listening on port " + port)
+    console.log(`Listening on port ${port}`)
+})
+
